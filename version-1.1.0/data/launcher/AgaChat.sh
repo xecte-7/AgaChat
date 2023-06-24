077810732e9a3ccd56f0501c783c4b0d362a9175
XECTE-7 <root.muhammadrizky98@gmail.com>
2023-06-24 20:40:35 +0800

    Releasing AgaChat v1.1.0 (2023-06-24 at 20:40)

---
+++ version-1.1.0/data/launcher/AgaChat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# AgaChat version 1.1.0 for Linux
+# Coded by Muhammad Rizky (XECTE-7)
+# repository : https://github.com/xecte-7/AgaChat
+
+cd /opt/AgaChat/
+exec "/opt/AgaChat/AgaChat"
